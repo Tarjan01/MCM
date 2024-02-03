14e015e3848cddb1678e2df394c8d8e8d8feb2db
Tarjan01 <743114342@qq.com>
2024-02-03 10:54:05 +0800

    denoisedByDFT.py final version

--- denoisedByDFT.py
+++ denoisedByDFT.py
@@ -1,7 +1,7 @@
 ## use DFT to denoise the data
-## to do: 
-## 1. find the optimal threshold of DFT
-## 2. explain why denoised data is better(graphs, etc.)
+## the optimal threshold is 726, which is 3times of the mean of the absolute value of the frequency
+## the plot of the frequency before and after denoised  can show the effect of the denoising
+## the output is saved in the output.txt, which contains the denoised data of p1 and p2
 
 import numpy as np
 import pandas as pd
@@ -21,35 +21,44 @@ from scipy.fftpack import fft, ifft
 data_denoised = np.zeros(len(p1_points_won))
 np.set_printoptions(formatter={'float': '{:.6f}'.format})
 
+def res_show():
+    original_data = p1_points_won - p2_points_won
+    denoised_data = data_denoised
+
+    fft_before = np.fft.fft(original_data)
+    fft_after = np.fft.fft(denoised_data)
+    freq = np.fft.fftfreq(len(original_data))
+    
+    print(3*np.abs(fft_before).mean())
+    
+    # plt.figure(figsize=(12, 6))
+    # plt.subplot(1, 2, 1)
+    # plt.plot(freq, np.abs(fft_before))
+    # plt.title('freq_before')
+    # plt.subplot(1, 2, 2)
+    # plt.plot(freq, np.abs(fft_after))
+    # plt.title('freq_after')
+    # plt.show()
+    sns.set(style="whitegrid")
+    plt.figure(figsize=(12, 6))
+    plt.subplot(1, 2, 1)
+    sns.lineplot(x=freq, y=np.abs(fft_before))
+    plt.title('freq_before_denoised')
+    plt.subplot(1, 2, 2)
+    sns.lineplot(x=freq, y=np.abs(fft_after))
+    plt.title('freq_after_denosied')
+    plt.show()
+
 def fit(start, end):
     p1 = p1_points_won[start:end]
     p2 = p2_points_won[start:end]
-    # with open('output.txt', 'w') as f:
-    #     print(p1 - p2, file=f)
     data_fft = fft(p1 - p2)
-    # data_fft_len = np.abs(data_fft)
-    # plt.figure(figsize=(12, 6))
-    # plt.plot(np.arange(len(data_fft_len)), np.abs(data_fft))
-    threshold = 10
+    threshold = 726
     data_fft_abs = np.abs(data_fft)
     data_fft[data_fft_abs < threshold] = 0
     data_denoised[start:end] = np.real(ifft(data_fft))
     # with open('output.txt', 'w') as f:
     #     print(data_denoised[start:end], file=f)
-        
-    # original_data = p1 - p2
-    # denoised_data = data_denoised[start:end]
-
-    # plt.figure(figsize=(12, 6))
-    # print(original_data, denoised_data)
-    # x = np.arange(len(original_data))
-    # plt.plot(x, original_data, label='Original')
-    # plt.plot(x, denoised_data, label='Denoised')
-    # # sns.lineplot(x=np.arange(len(original_data)), y=original_data, label='Original')
-    # # sns.lineplot(x=range(len(denoised_data)), y=denoised_data, label='Denoised')
-    # plt.legend()
-    # plt.title('DFT Denoising Result')
-    # plt.show()
     
 pre = 0
 for i in range(len(p1_points_won)):
@@ -64,6 +73,8 @@ fit(pre, len(p1_points_won))
 ## p1_denoised + p2_denoised = p1_points_won + p2_points_won
 p1_denoised = (p1_points_won + p2_points_won + data_denoised) / 2.0
 p2_denoised = (p2_points_won + p2_points_won - data_denoised) / 2.0
+
+res_show()
 
 with open('output.txt', 'w') as f:
     for i in range(len(p1_denoised)):
